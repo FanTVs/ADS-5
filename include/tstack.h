39ab4e7408139fc59679e129ab49130fc1fa1223
FanTVs <160307521+FanTVs@users.noreply.github.com>
2024-04-05 22:46:17 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,15 +1,18 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
  private:
-    T data[size];
+    T*data
     int top;
 
  public:
-    TStack() :top(0) {}
+    TStack() :top(0) {
+     data = new T[size];
+    }
     void push(const T& item) {
         if (top <= size - 1) {
             data[top++] = item;
